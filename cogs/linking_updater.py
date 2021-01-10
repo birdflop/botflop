aa1f2186c0dc9d7c0629c9d8c3f6971607c26dcf
Purpur <43528123+Pemigrade@users.noreply.github.com>
2021-01-10 06:03:19 -0600

    Update linking_updater.py

--- cogs/linking_updater.py
+++ cogs/linking_updater.py
@@ -21,97 +21,97 @@ class Linking_updater(commands.Cog):
         self.subuser_role_id = int(os.getenv('subuser_role_id'))
         self.verified_role_id = int(os.getenv('verified_role_id'))
 
-        @tasks.loop(minutes=10)
-        async def linking_updater(self):
-            logging.info("Synchronizing roles")
-            file = open('users.json', 'r')
-            data = json.load(file)
-            file.close()
-            guild = self.bot.get_guild(guild_id)
-            i = -1
-            print("1")
-            for client in data['users']:
-                print("2")
-                i += 1
-                member = guild.get_member(client['discord_id'])
-                if member:
-                    print("3")
-                    api_key = client['client_api_key']
-                    url = "https://panel.birdflop.com/api/client"
-                    headers = {
-                        'Accept': 'application/json',
-                        'Authorization': 'Bearer ' + api_key,
-                    }
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(url, headers=headers) as response:
-                            print("4")
-                            if response.status == 200:
-                                print("5")
-                                # Formats response for servers in JSON format
-                                servers_json_response = await response.json()
+    @tasks.loop(minutes=10)
+    async def linking_updater(self):
+        logging.info("Synchronizing roles")
+        file = open('users.json', 'r')
+        data = json.load(file)
+        file.close()
+        guild = self.bot.get_guild(guild_id)
+        i = -1
+        print("1")
+        for client in data['users']:
+            print("2")
+            i += 1
+            member = guild.get_member(client['discord_id'])
+            if member:
+                print("3")
+                api_key = client['client_api_key']
+                url = "https://panel.birdflop.com/api/client"
+                headers = {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + api_key,
+                }
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url, headers=headers) as response:
+                        print("4")
+                        if response.status == 200:
+                            print("5")
+                            # Formats response for servers in JSON format
+                            servers_json_response = await response.json()
 
-                                user_client = False
-                                user_subuser = False
-                                user_crabwings = False
-                                user_duckfeet = False
-                                user_elktail = False
-                                for server in servers_json_response['data']:
-                                    server_owner = server['attributes']['server_owner']
-                                    if server_owner == True:
-                                        user_client = True
-                                    elif server_owner == False:
-                                        user_subuser = True
-                                    server_node = server['attributes']['node']
-                                    if server_node == "Crabwings - NYC":
-                                        user_crabwings = True
-                                    elif server_node == "Duckfeet - EU":
-                                        user_duckfeet = True
-                                    elif server_node == "Elktail - EU":
-                                        user_elktail = True
-                                role = discord.utils.get(guild.roles, id=self.client_role_id)
-                                if user_client == True:
-                                    await member.add_roles(role)
-                                else:
-                                    await member.remove_roles(role)
-                                role = discord.utils.get(guild.roles, id=self.subuser_role_id)
-                                if user_subuser == True:
-                                    await member.add_roles(role)
-                                else:
-                                    await member.remove_roles(role)
-                                role = discord.utils.get(guild.roles, id=self.crabwings_role_id)
-                                if user_crabwings == True:
-                                    await member.add_roles(role)
-                                else:
-                                    await member.remove_roles(role)
-                                role = discord.utils.get(guild.roles, id=self.duckfeet_role_id)
-                                if user_duckfeet == True:
-                                    await member.add_roles(role)
-                                else:
-                                    await member.remove_roles(role)
-                                role = discord.utils.get(guild.roles, id=self.elktail_role_id)
-                                if user_elktail == True:
-                                    await member.add_roles(role)
-                                else:
-                                    await member.remove_roles(role)
+                            user_client = False
+                            user_subuser = False
+                            user_crabwings = False
+                            user_duckfeet = False
+                            user_elktail = False
+                            for server in servers_json_response['data']:
+                                server_owner = server['attributes']['server_owner']
+                                if server_owner == True:
+                                    user_client = True
+                                elif server_owner == False:
+                                    user_subuser = True
+                                server_node = server['attributes']['node']
+                                if server_node == "Crabwings - NYC":
+                                    user_crabwings = True
+                                elif server_node == "Duckfeet - EU":
+                                    user_duckfeet = True
+                                elif server_node == "Elktail - EU":
+                                    user_elktail = True
+                            role = discord.utils.get(guild.roles, id=self.client_role_id)
+                            if user_client == True:
+                                await member.add_roles(role)
                             else:
-                                data['users'].pop(i)
-                                json_dumps = json.dumps(data, indent=2)
-                                file = open('users.json', 'w')
-                                file.write(json_dumps)
-                                file.close()
-                                await member.edit(roles=[])
-                                logging.info("removed discord_id " + str(client['discord_id']) + " with client_id " + str(
-                                    client['client_id']) + " and INVALID client_api_key " + client['client_api_key'])
-                else:
-                    data['users'].pop(i)
-                    json_dumps = json.dumps(data, indent=2)
-                    file = open('users.json', 'w')
-                    file.write(json_dumps)
-                    file.close()
-                    logging.info("removed discord_id " + str(client['discord_id']) + " with client_id " + str(
-                        client['client_id']) + " and client_api_key " + client['client_api_key'])
-        
-        @linking_updater.before_loop
-        async def before_linking_updater(self):
-            logging.info('waiting to enter loop')
-            await bot.wait_until_ready()
+                                await member.remove_roles(role)
+                            role = discord.utils.get(guild.roles, id=self.subuser_role_id)
+                            if user_subuser == True:
+                                await member.add_roles(role)
+                            else:
+                                await member.remove_roles(role)
+                            role = discord.utils.get(guild.roles, id=self.crabwings_role_id)
+                            if user_crabwings == True:
+                                await member.add_roles(role)
+                            else:
+                                await member.remove_roles(role)
+                            role = discord.utils.get(guild.roles, id=self.duckfeet_role_id)
+                            if user_duckfeet == True:
+                                await member.add_roles(role)
+                            else:
+                                await member.remove_roles(role)
+                            role = discord.utils.get(guild.roles, id=self.elktail_role_id)
+                            if user_elktail == True:
+                                await member.add_roles(role)
+                            else:
+                                await member.remove_roles(role)
+                        else:
+                            data['users'].pop(i)
+                            json_dumps = json.dumps(data, indent=2)
+                            file = open('users.json', 'w')
+                            file.write(json_dumps)
+                            file.close()
+                            await member.edit(roles=[])
+                            logging.info("removed discord_id " + str(client['discord_id']) + " with client_id " + str(
+                                client['client_id']) + " and INVALID client_api_key " + client['client_api_key'])
+            else:
+                data['users'].pop(i)
+                json_dumps = json.dumps(data, indent=2)
+                file = open('users.json', 'w')
+                file.write(json_dumps)
+                file.close()
+                logging.info("removed discord_id " + str(client['discord_id']) + " with client_id " + str(
+                    client['client_id']) + " and client_api_key " + client['client_api_key'])
+
+    @linking_updater.before_loop
+    async def before_linking_updater(self):
+        logging.info('waiting to enter loop')
+        await bot.wait_until_ready()
