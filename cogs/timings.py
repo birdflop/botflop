3d45c2d8a453d59aa0525075c71686890bb2c33e
granny <pkrok01@outlook.com>
2021-01-12 18:57:25 -0800

    add more to timings_check .yml

--- cogs/timings.py
+++ cogs/timings.py
@@ -65,62 +65,35 @@ class Timings(commands.Cog):
             try:
                 version = r["timingsMaster"]["version"].lower()
                 if "version" in TIMINGS_CHECK:
-                    if TIMINGS_CHECK["version"] in version:
+                    if TIMINGS_CHECK["version"] not in version:
                         embed_var.add_field(name="❌ Legacy Build",
-                                            value="Update to " + TIMINGS_CHECK["server"]["version"])
+                                            value="Update to " + TIMINGS_CHECK["version"])
                 if "servers" in TIMINGS_CHECK:
                     for server in TIMINGS_CHECK["servers"]:
                         if server["name"] in version:
                             embed_var.add_field(**create_field(server))
                             break
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                if "config" in TIMINGS_CHECK:
-                    server_properties = None
-                    bukkit = None
-                    spigot = None
-                    paper = None
-                    purpur = None
-                    if "server.properties" in r["timingsMaster"]["config"]:
-                        server_properties = r["timingsMaster"]["config"]["server.properties"]
-                    if "bukkit" in r["timingsMaster"]["config"]:
-                        bukkit = r["timingsMaster"]["config"]["bukkit"]
-                    if "spigot" in r["timingsMaster"]["config"]:
-                        spigot = r["timingsMaster"]["config"]["spigot"]
-                    if "paper" in r["timingsMaster"]["config"]:
-                        paper = r["timingsMaster"]["config"]["paper"]
-                    if "purpur" in r["timingsMaster"]["config"]:
-                        purpur = r["timingsMaster"]["config"]["purpur"]
-
-                    for config_name in TIMINGS_CHECK["config"]:
-                        config = TIMINGS_CHECK["config"][config_name]
-                        for option_name in config:
-                            option = config[option_name]
-                            for expression in option["expressions"]:
-                                if not eval(expression):
-                                    continue
-                                embed_var.add_field(**create_field({**{"name": option_name}, **option}))
-
-            except KeyError:
-                unchecked = unchecked + 1
+            except KeyError as key:
+                print("Missing: " + str(key))
+                unchecked += 1
 
             try:
                 timing_cost = int(r["timingsMaster"]["system"]["timingcost"])
                 if timing_cost > 300:
                     embed_var.add_field(name="❌ Timingcost",
                                         value="Your timingcost is " + str(timing_cost) + ". Find a [better host](https://www.birdflop.com).")
-            except KeyError:
-                unchecked = unchecked + 1
+            except KeyError as key:
+                print("Missing: " + str(key))
+                unchecked += 1
 
             try:
                 jvm_version = r["timingsMaster"]["system"]["jvmversion"]
                 if jvm_version.startswith("1.8.") or jvm_version.startswith("9.") or jvm_version.startswith("10."):
                     embed_var.add_field(name="❌ Java Version",
                                         value="You are using Java " + jvm_version + ". Update to [Java 11](https://adoptopenjdk.net/installation.html).")
-            except KeyError:
-                unchecked = unchecked + 1
+            except KeyError as key:
+                print("Missing: " + str(key))
+                unchecked += 1
 
             try:
                 flags = r["timingsMaster"]["system"]["flags"]
@@ -169,8 +142,10 @@ class Timings(commands.Cog):
                 else:
                     embed_var.add_field(name="❌ Aikar's Flags",
                                         value="Use [Aikar's flags](https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/).")
-            except KeyError:
-                unchecked = unchecked + 1
+            except KeyError as key:
+                print("Missing: " + str(key))
+                unchecked += 1
+
             try:
                 cpu = int(r["timingsMaster"]["system"]["cpu"])
                 if cpu == 1:
@@ -179,162 +154,52 @@ class Timings(commands.Cog):
                 if cpu == 2:
                     embed_var.add_field(name="❌ Threads",
                                         value="You have only " + str(cpu) + " threads. Find a [better host](https://www.birdflop.com).")
-            except KeyError:
-                unchecked = unchecked + 1
+            except KeyError as key:
+                print("Missing: " + str(key))
+                unchecked += 1
 
-            try:
-                plugins = r["timingsMaster"]["plugins"]
-                if not YAML_ERROR:
+            plugins = r["timingsMaster"]["plugins"] if "plugins" in r["timingsMaster"] else None
+            server_properties = r["timingsMaster"]["config"]["server.properties"] if "server.properties" in r["timingsMaster"]["config"] else None
+            bukkit = r["timingsMaster"]["config"]["bukkit"] if "bukkit" in r["timingsMaster"]["config"] else None
+            spigot = r["timingsMaster"]["config"]["spigot"] if "spigot" in r["timingsMaster"]["config"] else None
+            paper = r["timingsMaster"]["config"]["paper"] if "paper" in r["timingsMaster"]["config"] else None
+            purpur = r["timingsMaster"]["config"]["purpur"] if "purpur" in r["timingsMaster"]["config"] else None
+            if not YAML_ERROR:
+                if "plugins" in TIMINGS_CHECK:
                     for server_name in TIMINGS_CHECK["plugins"]:
                         if server_name in r["timingsMaster"]["config"]:
                             for plugin in plugins:
                                 for plugin_name in TIMINGS_CHECK["plugins"][server_name]:
                                     if plugin == plugin_name:
                                         stored_plugin = TIMINGS_CHECK["plugins"][server_name][plugin_name]
-                                        stored_plugin["name"] = plugin_name
-                                        embed_var.add_field(**create_field(stored_plugin))
-                else:
-                    embed_var.add_field(name="Error loading YAML file",
-                                        value=YAML_ERROR)
-                for plugin in plugins:
-                    if "songoda" in r["timingsMaster"]["plugins"][plugin]["authors"].casefold():
-                        if plugin == "EpicHeads":
-                            embed_var.add_field(name="❌ EpicHeads",
-                                                value="This plugin was made by Songoda. Songoda resources are poorly developed and often cause problems. You should find an alternative such as [HeadsPlus](spigotmc.org/resources/headsplus-»-1-8-1-16-4.40265/) or [HeadDatabase](https://www.spigotmc.org/resources/head-database.14280/).")
-                        elif plugin == "UltimateStacker":
-                            embed_var.add_field(name="❌ UltimateStacker",
-                                                value="Stacking plugins actually causes more lag. "
-                                                      "Remove UltimateStacker.")
-                        else:
-                            embed_var.add_field(name="❌ " + plugin,
-                                                value="This plugin was made by Songoda. Songoda resources are poorly developed and often cause problems. You should find an alternative.")
-            except KeyError:
-                unchecked = unchecked + 1
+                                        if isinstance(stored_plugin, dict):
+                                            stored_plugin["name"] = plugin_name
+                                            embed_var.add_field(**create_field(stored_plugin))
+                                        else:
+                                            eval_field(embed_var, stored_plugin, plugin_name, unchecked, plugins, server_properties, bukkit, spigot, paper, purpur)
+                                if "songoda" in r["timingsMaster"]["plugins"][plugin]["authors"].casefold():
+                                    if plugin == "EpicHeads":
+                                        embed_var.add_field(name="❌ EpicHeads",
+                                                            value="This plugin was made by Songoda. Songoda resources are poorly developed and often cause problems. You should find an alternative such as [HeadsPlus](spigotmc.org/resources/headsplus-»-1-8-1-16-4.40265/) or [HeadDatabase](https://www.spigotmc.org/resources/head-database.14280/).")
+                                    elif plugin == "UltimateStacker":
+                                        embed_var.add_field(name="❌ UltimateStacker",
+                                                            value="Stacking plugins actually causes more lag. "
+                                                                    "Remove UltimateStacker.")
+                                    else:
+                                        embed_var.add_field(name="❌ " + plugin,
+                                                            value="This plugin was made by Songoda. Songoda resources are poorly developed and often cause problems. You should find an alternative.")
+                if "config" in TIMINGS_CHECK:
+                    for config_name in TIMINGS_CHECK["config"]:
+                        config = TIMINGS_CHECK["config"][config_name]
+                        for option_name in config:
+                            option = config[option_name]
+                            eval_field(embed_var, option, option_name, unchecked, plugins, server_properties, bukkit, spigot, paper, purpur)
+            else:
+                embed_var.add_field(name="Error loading YAML file",
+                                    value=YAML_ERROR)
 
-            try:
-                plugins = r["timingsMaster"]["plugins"]
-                if "PhantomSMP" in plugins:
-                    phantoms_only_insomniacs = r["timingsMaster"]["config"]["paper"]["world-settings"]["default"][
-                        "phantoms-only-attack-insomniacs"]
-                    if phantoms_only_insomniacs == "false":
-                        embed_var.add_field(name="❌ PhantomSMP",
-                                            value="You probably don't need PhantomSMP as Paper already has its features. "
-                                                  "Remove PhantomSMP.")
-                    else:
-                        embed_var.add_field(name="❌ PhantomSMP",
-                                            value="You probably don't need PhantomSMP as Paper already has its features. "
-                                                  "Enable phantoms-only-attack-insomniacs in paper.yml.")
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                network_compression_threshold = int(
-                    r["timingsMaster"]["config"]["server.properties"]["network-compression-threshold"])
-                bungeecord = r["timingsMaster"]["config"]["spigot"]["settings"]["bungeecord"]
-                if network_compression_threshold <= 256 and bungeecord == "false":
-                    embed_var.add_field(name="❌ network-compression-threshold",
-                                        value="Increase this in server.properties. Recommended: 512.")
-                if network_compression_threshold != -1 and bungeecord == "true":
-                    embed_var.add_field(name="❌ network-compression-threshold",
-                                        value="Set this to -1 in server.properties for a bungee server like yours.")
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                mob_spawn_range = int(r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["mob-spawn-range"])
-                spigot_view_distance = r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["view-distance"]
-                if spigot_view_distance == "default":
-                    view_distance = int(r["timingsMaster"]["config"]["server.properties"]["view-distance"])
-                    if mob_spawn_range >= 8 and view_distance <= 6:
-                        embed_var.add_field(name="❌ mob-spawn-range",
-                                            value="Decrease this in spigot.yml. "
-                                                  "Recommended: " + str(view_distance - 1) + ".")
-                elif mob_spawn_range >= 8 and int(spigot_view_distance) <= 6:
-                            embed_var.add_field(name="❌ mob-spawn-range",
-                                                value="Decrease this in spigot.yml. "
-                                                      "Recommended: " + str(int(spigot_view_distance) - 1) + ".")
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                raiders_entity_activation_range = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "raiders"])
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                flying_monsters_entity_activation_range = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "flying-monsters"])
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                wake_up_inactive_villagers_every = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "wake-up-inactive"]["villagers-every"])
-            except KeyError:
-                unchecked = unchecked + 1
-
-
-            try:
-                wake_up_inactive_animals_every = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "wake-up-inactive"]["animals-every"])
-
-            except KeyError:
-                unchecked = unchecked + 1
-
-
-
-            try:
-                wake_up_inactive_flying_monsters_every = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "wake-up-inactive"]["flying-monsters-every"])
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                wake_up_inactive_monsters_every = int(
-                    r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"]["entity-activation-range"][
-                        "wake-up-inactive"]["monsters-every"])
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                no_tick_view_distance = int(
-                    r["timingsMaster"]["config"]["paper"]["world-settings"]["default"]["viewdistances"][
-                        "no-tick-view-distance"])
-                if no_tick_view_distance == -1:
-                    spigot_view_distance = r["timingsMaster"]["config"]["spigot"]["world-settings"]["default"][
-                        "view-distance"]
-                    if spigot_view_distance == "default":
-                        view_distance = int(r["timingsMaster"]["config"]["server.properties"]["view-distance"])
-                        if view_distance >= 4:
-                            embed_var.add_field(name="❌ no-tick-view-distance",
-                                                value="Set a value in paper.yml. "
-                                                      "Recommended: " + str(view_distance) + ". And reduce view-distance in server.properties. Recommended: 3.")
-                    elif int(spigot_view_distance) >= 4:
-                        embed_var.add_field(name="❌ no-tick-view-distance",
-                                            value="Set a value in paper.yml. "
-                                                  "Recommended: " + spigot_view_distance + ". And reduce view-distance in spigot.yml. Recommended: 3.")
-            except KeyError:
-                unchecked = unchecked + 1
-
-            try:
-                use_alternate_keepalive = r["timingsMaster"]["config"]["purpur"]["settings"]["use-alternate-keepalive"]
-                plugins = r["timingsMaster"]["plugins"]
-                if use_alternate_keepalive == "false" and "TCPShield" not in plugins:
-                    embed_var.add_field(name="❌ use-alternate-keepalive",
-                                        value="Enable this in purpur.yml.")
-                if use_alternate_keepalive == "true" and "TCPShield" in plugins:
-                    embed_var.add_field(name="❌ use-alternate-keepalive",
-                                        value="Disable this in purpur.yml. It causes issues with TCPShield.")
-            except KeyError:
-                unchecked = unchecked + 1
-
-        except ValueError:
+        except ValueError as bruh:
+            print(bruh)
             embed_var.add_field(name="❌ Invalid Configuration",
                                 value="At least one of your configuration files had an invalid data type.")
 
@@ -348,8 +213,31 @@ class Timings(commands.Cog):
         if issue_count >= 25:
             embed_var.insert_field_at(index=24, name="Plus " + str(issue_count - 24) + " more recommendations", value="Create a new timings report after resolving some of the above issues to see more.")
         if unchecked > 0:
-            embed_var.description = "||" + str(unchecked) + " missing configuration optimizations due to your server version.||"
+            embed_var.description = "||" + str(unchecked) + " missing configuration optimizations.||"
         await message.reply(embed=embed_var)
+
+def eval_field(embed_var, option, option_name, unchecked, plugins=None, server_properties=None, bukkit=None, spigot=None, paper=None, purpur=None):
+    try:
+        for option_data in option:
+            add_to_field = True
+            for expression in option_data["expressions"]:
+                if ("server_properties" in expression and server_properties or
+                    "bukkit" in expression and bukkit or
+                    "spigot" in expression and spigot or
+                    "paper" in expression and paper or
+                    "purpur" in expression and purpur or
+                    "plugins" in expression and plugins):
+                    if not eval(expression):
+                        add_to_field = False
+                        break
+            if add_to_field:
+                """ f strings don't like newlines so we replace the newlines with placeholder text before we eval """
+                option_data["value"] = eval('f"""' + option_data["value"].replace("\n", "\\|n\\") + '"""').replace("\\|n\\", "\n")
+                embed_var.add_field(**create_field({**{"name": option_name}, **option_data}))
+                break
+    except KeyError as key:
+        print("Missing: " + str(key))
+        unchecked += 1
 
 def create_field(option):
     field = {"name": option["name"],
