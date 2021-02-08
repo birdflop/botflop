40c8e8ff7e5921c2f39c9c28632fca729b278838
Purpur <43528123+Pemigrade@users.noreply.github.com>
2021-02-08 17:31:35 -0600

    Fix md

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def ping(ctx):
 
 @bot.command()
 async def invite(ctx):
-    await ctx.send('Invite me with this link:/nhttps://discord.com/oauth2/authorize?client_id=787929894616825867&permissions=0&scope=bot')
+    await ctx.send('Invite me with this link:\nhttps://discord.com/oauth2/authorize?client_id=787929894616825867&permissions=0&scope=bot')
 
 @bot.command(name="react", pass_context=True)
 @has_permissions(administrator=True)
