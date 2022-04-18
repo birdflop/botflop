e9579c41dc5d594b5dce857a65e4fc49294c8b2f
Saboor <saboor547@gmail.com>
2022-04-18 16:16:08 -0600

    fix message.update error

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,5 +2,5 @@ const analyzeTimings = require('../functions/analyzeTimings');
 module.exports = async (client, message) => {
 	if (message.author.bot) return;
 	const timings = await analyzeTimings(message, client, message.content.split(' '));
-	if (timings) await message.update(timings);
+	if (timings) await message.reply(timings);
 };
