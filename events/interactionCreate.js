996649941a7fe94deb2c6843e5a3d3294f9a223f
Saboor <saboor547@gmail.com>
2022-04-18 16:29:48 -0600

    update packages and remove pretty-error cuz stupid

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,28 +4,26 @@ function clean(text) {
 	else return text;
 }
 module.exports = async (client, interaction) => {
-	if (interaction.isButton()) {
-		const button = client.buttons.get(interaction.customId);
-		if (!button) return interaction.reply({ content: 'Unknown Button!', ephemeral: true });
+	if (!interaction.isButton()) return;
+	const button = client.buttons.get(interaction.customId);
+	if (!button) return interaction.reply({ content: 'Unknown Button!', ephemeral: true });
 
-		interaction.deferUpdate();
+	interaction.deferUpdate();
 
-		try {
-			client.logger.info(`${interaction.user.tag} clicked button: ${button.name}, in ${interaction.guild.name}`);
-			button.execute(interaction, client);
-		}
-		catch (error) {
-			const interactionFailed = new MessageEmbed()
-				.setColor(Math.floor(Math.random() * 16777215))
-				.setTitle('INTERACTION FAILED')
-				.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL({ dynamic : true }) })
-				.addField('**Type:**', 'Button')
-				.addField('**Interaction:**', button.name)
-				.addField('**Error:**', clean(error));
-			if (interaction.guild) interactionFailed.addField('**Guild:**', interaction.guild.name).addField('**Channel:**', interaction.channel.name);
-			client.users.cache.get('249638347306303499').send({ embeds: [interactionFailed] });
-			interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
-			client.logger.error(error);
-		}
+	try {
+		client.logger.info(`${interaction.user.tag} clicked button: ${button.name}, in ${interaction.guild.name}`);
+		button.execute(interaction, client);
+	}
+	catch (error) {
+		const interactionFailed = new MessageEmbed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('INTERACTION FAILED')
+			.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL({ dynamic : true }) })
+			.addField('**Interaction:**', button.name)
+			.addField('**Error:**', clean(error))
+			.addField('**Guild:**', interaction.guild.name)
+			.addField('**Channel:**', interaction.channel.name);
+		interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
+		client.logger.error(error);
 	}
 };
--- handlers/!log.js
+++ handlers/!log.js
@@ -1,10 +1,8 @@
 const { createLogger, format, transports } = require('winston');
 const rn = new Date();
 const date = `${minTwoDigits(rn.getMonth() + 1)}-${minTwoDigits(rn.getDate())}-${rn.getFullYear()}`;
-const time = `${minTwoDigits(rn.getHours())}-${minTwoDigits(rn.getMinutes())}`;
 function minTwoDigits(n) { return (n < 10 ? '0' : '') + n; }
 module.exports = client => {
-	require('pretty-error').start();
 	client.logger = createLogger({
 		format: format.combine(
 			format.colorize(),
@@ -13,11 +11,11 @@ module.exports = client => {
 		),
 		transports: [
 			new transports.Console(),
-			new transports.File({ filename: `logs/info/${date}/${time}.log` }),
+			new transports.File({ filename: `logs/${date}.log` }),
 		],
 		rejectionHandlers: [
 			new transports.Console(),
-			new transports.File({ filename: `logs/error/${date}.log` }),
+			new transports.File({ filename: `logs/${date}.log` }),
 		],
 	});
 	client.logger.info('Logger started');
