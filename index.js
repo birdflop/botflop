ebad2fcc82108d2016e97af4c3dc565356afeb7e
Purpur <pemigrade@gmail.com>
2022-06-25 15:32:40 -0400

    messagecontent app not approved yet

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.MessageContent,
+		// GatewayIntentBits.MessageContent,
 	],
 });
 client.startTimestamp = Date.now();
