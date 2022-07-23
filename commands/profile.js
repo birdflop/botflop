8812e63a79e78f3fda61623891de590860d2fec1
saboooor <saboor547@gmail.com>
2022-07-22 20:55:36 -0600

    refactoring

--- commands/profile.js
+++ commands/profile.js
@@ -4,32 +4,31 @@ module.exports = {
 	name: 'profile',
 	description: 'Analyze spark profiles to help optimize your server.',
 	args: true,
-	usage: '<Spark Link>',
+	usage: '<Spark Profile Link>',
 	options: [{
 		'type': ApplicationCommandOptionType.String,
 		'name': 'url',
-		'description': 'The Spark URL',
+		'description': 'The Spark Profile URL',
 		'required': true,
 	}],
 	async execute(message, args, client) {
 		try {
-			const timingsresult = await analyzeProfile(message, client, args);
-            return
-			const timingsmsg = await message.reply(timingsresult ? timingsresult[0] : 'Invalid Timings URL.');
-			if (!timingsresult) return;
+			const profileresult = await analyzeProfile(message, client, args);
+			const profilemsg = await message.reply(profileresult ? profileresult[0] : 'Invalid Profile URL.');
+			if (!profileresult) return;
 
-			// Get the issues from the timings result
-			const issues = timingsresult[1];
+			// Get the issues from the profile result
+			const issues = profileresult[1];
 			if (issues) {
-				const filter = i => i.user.id == (message.author ?? message.user).id && i.customId.startsWith('timings_');
-				const collector = timingsmsg.createMessageComponentCollector({ filter, time: 300000 });
+				const filter = i => i.user.id == (message.author ?? message.user).id && i.customId.startsWith('profile_');
+				const collector = profilemsg.createMessageComponentCollector({ filter, time: 300000 });
 				collector.on('collect', async i => {
 					// Defer button
 					i.deferUpdate();
 
 					// Get the embed
-					const TimingsEmbed = new EmbedBuilder(i.message.embeds[0].toJSON());
-					const footer = TimingsEmbed.toJSON().footer;
+					const ProfileEmbed = new EmbedBuilder(i.message.embeds[0].toJSON());
+					const footer = ProfileEmbed.toJSON().footer;
 
 					// Calculate total amount of pages and get current page from embed footer
 					const text = footer.text.split(' • ');
@@ -37,19 +36,19 @@ module.exports = {
 					const maxPages = parseInt(text[text.length - 1].split('Page ')[1].split(' ')[2]);
 
 					// Get next page (if last page, go to pg 1)
-					const page = i.customId == 'timings_next' ? lastPage == maxPages ? 1 : lastPage + 1 : lastPage - 1 ? lastPage - 1 : maxPages;
+					const page = i.customId == 'profile_next' ? lastPage == maxPages ? 1 : lastPage + 1 : lastPage - 1 ? lastPage - 1 : maxPages;
 					const end = page * 12;
 					const start = end - 12;
 					const fields = issues.slice(start, end);
 
 					// Update the embed
 					text[text.length - 1] = `Page ${page} of ${Math.ceil(issues.length / 12)}`;
-					TimingsEmbed
+					ProfileEmbed
 						.setFields(fields)
 						.setFooter({ iconURL: footer.icon_url, text: text.join(' • ') });
 
 					// Send the embed
-					timingsmsg.edit({ embeds: [TimingsEmbed] });
+					profilemsg.edit({ embeds: [ProfileEmbed] });
 				});
 			}
 		}
--- functions/analyzeProfile.js
+++ functions/analyzeProfile.js
@@ -3,21 +3,22 @@ const YAML = require('yaml');
 const fs = require('fs');
 const createField = require('./createField.js');
 const evalField = require('./evalField.js');
-const Pbf = require("pbf");
-const { HeapData, SamplerData } = require("../protos");
-module.exports = async function analyzeProfile(message, client, args) {
-
-    const url = args[0];
-    const code = url.replace("https://spark.lucko.me/", "");
-    const bytebin = `https://bytebin.lucko.me/${code}`;
-    const req = await fetch(bytebin);
-    const buf = await req.arrayBuffer();
-    const sampler = parse(buf, SamplerData);
-    console.log(sampler);
-};
+const Pbf = require('pbf');
+const { HeapData, SamplerData } = require('../protos');
 
 // Function to parse the data payload from a request given the schema type
 function parse(buf, schema) {
-    const pbf = new Pbf(new Uint8Array(buf));
-    return schema.read(pbf);
+	const pbf = new Pbf(new Uint8Array(buf));
+	return schema.read(pbf);
 }
+
+module.exports = async function analyzeProfile(message, client, args) {
+
+	const url = args[0];
+	const code = url.replace('https://spark.lucko.me/', '');
+	const bytebin = `https://bytebin.lucko.me/${code}`;
+	const req = await fetch(bytebin);
+	const buf = await req.arrayBuffer();
+	const sampler = parse(buf, SamplerData);
+	console.log(sampler);
+};
