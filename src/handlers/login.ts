f2043939a079e5ca377043c4b8559a150ef2f3c7
saboooor <saboor547@gmail.com>
2026-09-02 00:55:11 -0400

    h

--- src/handlers/login.ts
+++ src/handlers/login.ts
@@ -1,6 +1,6 @@
 import { Client } from 'discord.js';
 
 export default (client: Client) => {
-  client.login(process.env.BOT_TOKEN);
+  client.login(process.env.TOKEN);
   logger.info('Bot logged in');
 };
